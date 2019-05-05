1c8c5807ea0678fcca321b4eb781c5f28dd95fdf
samsonjj <samsonjj@vcu.edu>
2019-05-05 17:40:03 -0400

    new deploy script

--- deploy.php
+++ deploy.php
@@ -3,21 +3,21 @@
 	 * GIT DEPLOYMENT SCRIPT
 	 *
 	 * Used for automatically deploying websites via github or bitbucket, more deets here:
-	 * https://gist.github.com/riodw/71f6e2244534deae652962b32b7454e2
-	 * How To Use:
-	 * https://medium.com/riow/deploy-to-production-server-with-git-using-php-ab69b13f78ad
+	 *
+	 *		https://gist.github.com/1809044
 	 */
+
 	// The commands
 	$commands = array(
 		'echo $PWD',
 		'whoami',
-		'git reset --hard HEAD',
 		'git pull',
 		'git status',
 		'git submodule sync',
 		'git submodule update',
 		'git submodule status',
 	);
+
 	// Run the commands for output
 	$output = '';
 	foreach($commands AS $command){
@@ -27,6 +27,7 @@
 		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
 		$output .= htmlentities(trim($tmp)) . "\n";
 	}
+
 	// Make it pretty for manual user access (and why not?)
 ?>
 <!DOCTYPE HTML>
@@ -37,11 +38,11 @@
 </head>
 <body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
 <pre>
- ____________________________
-|                            |
-| Git Deployment Script v0.1 |
-|      github.com/riodw 2019 |
-|____________________________|
+ .  ____  .    ____________________________
+ |/      \|   |                            |
+[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
+ |___==___|  /              &copy; oodavid 2012 |
+              |____________________________|
 
 <?php echo $output; ?>
 </pre>
